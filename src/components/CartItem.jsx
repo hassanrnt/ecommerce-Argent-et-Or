85189a0dbc5a8355dab99e9a3a2f00117062b296
hassanrnt <hassanritouny97@gmail.com>
2024-01-22 16:01:36 +0100

    In another addition I added in Cartitem by increasing id,image,price,amount

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 
 const CartItem = ({item}) => {
-  return <div>{item.title}</div>;
+  // destructure item
+  const {id, title, image, price, amount} = item;
+  return <div>
+    <div>
+      {/* image */}
+      <div>
+        <img src={image} alt=''/>
+      </div>
+    </div>
+  </div>;
 };
 
 export default CartItem;
