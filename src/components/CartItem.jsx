797d93713c3a3445f6dcb3683602549861996050
hassanrnt <hassanritouny97@gmail.com>
2024-01-22 16:44:02 +0100

    I added the link property

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
-
+// import Link
+import { Link } from 'react-router-dom';
 const CartItem = ({item}) => {
   // destructure item
   const {id, title, image, price, amount} = item;
   return <div>
-    <div>
-      {/* image */}
-      <div>
-        <img src={image} alt=''/>
+    <div className='flex'>
+      <div className='w-full min-h-[150px] flex items-center gap-x-4'>
+        {/* image */}
+        <Link to={`/product/${id}`}>
+        <img className='max-w-[80px]' src={image} alt=''/>
+        </Link>
       </div>
     </div>
   </div>;
