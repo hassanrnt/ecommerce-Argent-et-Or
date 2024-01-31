d9f7c348ae130420578b0d603a2f9d447b49923c
hassanrnt <hassanritouny97@gmail.com>
2024-01-31 12:42:34 +0100

    add to Dicover more in border primary

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,12 +11,15 @@ const Hero = () => {
       <div className='flex flex-col justify-center'>
         {/* pretitle */}
         <div className='font-semibold flex items-center uppercase'>
-          <div>New Trend</div>
+          <div className='w-10 h-[2px] bg-red-500 mr-3'></div>NEW TREND
         </div>
         {/* title*/}
         <h1 className='text-[70px] leading-[1.1] font-light mb-4'>AUTUMN SALE STYLISH <br/>
         <span className='font-semibold'>WOMENS</span>
          </h1>
+         <Link to={'/'} className='self-start uppercase font-semibold border-b-2 border-primary'>
+          Discover More
+         </Link>
       </div>
       {/* image */}
       <div className='hidden lg:block'>
