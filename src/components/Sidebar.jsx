2b5247d098a7559d4604745dd842de028bb273f8
hassanrnt <hassanritouny97@gmail.com>
2024-02-01 17:32:46 +0100

    add to checkout

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -35,7 +35,7 @@ const Sidebar = () => {
           </div>
           </div>
           <Link  to='/'className='bg-gray-200 flex p-4 justify-center items-center text-primary w-full font-medium'>View Cart</Link>
-          <Link to='/' className=''>Checkout</Link>
+          <Link to='/' className='bg-pink-200 flex p-4 justify-center items-center text-white w-full font-medium'>Checkout</Link>
       </div>
     </div>
   );
