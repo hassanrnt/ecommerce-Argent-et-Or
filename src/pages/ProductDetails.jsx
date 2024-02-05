4a219431a655ff7135f1b2bdd4df71c5f410b175
hassanrnt <hassanritouny97@gmail.com>
2024-02-05 14:59:17 +0100

    add to pareInt

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,6 +1,25 @@
-import React from 'react';
+import React, {useContext} from 'react';
+// import useParams
+import {useParams} from 'react-router-dom';
+// import cart context
+import {CartContext} from '../contexts/CartContext';
+// import product context
+import {ProductContext} from '../contexts/ProductContext';
+
+
+
 
 const ProductDetails = () => {
+  // get the product id from the url
+  const {id} =useParams()
+  const {products}=useContext(ProductContext);
+  const {addToCart}=useContext(CartContext);
+  // get the single product based on the id
+  const product = products.find(item =>{
+    return item.id === parseInt(id);
+  })
+  console.log(product);
+
   return <div>Product Details Page</div>;
 };
 
