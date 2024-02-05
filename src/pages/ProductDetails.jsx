dcc436f7d38f73792ff61293e6f1339fd2982055
hassanrnt <hassanritouny97@gmail.com>
2024-02-05 15:33:32 +0100

    add to image

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -25,9 +25,22 @@ const ProductDetails = () => {
    return <section className='h-screen flex justify-center items-center'>
     Loading...</section>;
   }
-  console.log(product);
+   // destructure product
+   const {title, price, decription, image} = product;
+  return <section className='pt-32 pb lg:py-32 h-screen flex items-center'>
+    <div className="container mx-auro">
+      {/* image & text wrapper */}
+      <div className='flex flex-col lg:flex-row items-center'>
+        {/* image */}
+      <div className='flex flex-1 justify-center items-center mb-8 lg:mb-0'>
+        <img  className=' max-w-[200px] lg:max-w-sm'src={image} alt=''/>
+      </div>
+      {/* text */}
+      <div>text</div>
+      </div>
+    </div>
+  </section>;
 
-  return <div>Product Details Page</div>;
 };
 
 export default ProductDetails;
